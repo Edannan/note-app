a1216c6b553a3b571e47d9a69d6414fb1d1faaa5
Emmanuel Danso Annan <emmanueldansoannan@gmail.com>
2023-01-21 08:14:53 +0000

    text color change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     const year = date.getFullYear();
   return (
     <footer className='bg-gray-600 text-white flex justify-center items-center fixed bottom-0 w-full text-xs p-3'>
-    <p>Created By<a href="https://www.linkedin.com/in/emmanuel-danso-annan-2aa777226/"> Emmanuel Danso Annan </a>| ©️ {year} All rights reserved.</p>
+    <p>Created By<span className='hover:text-indigo-700'><a href="https://www.linkedin.com/in/emmanuel-danso-annan-2aa777226/"> Emmanuel Danso Annan </a></span>| ©️ {year} All rights reserved.</p>
     </footer>
   )
 }
